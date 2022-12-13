2dfebf1baf45545c489be6b3fe7fc91b3151510d
d-laliev@mail.ru <d-laliev@mail.ru>
2022-12-13 15:50:25 +0300

    Добавьте файлы проекта.

---
+++ find-a-couple/FormGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace find_a_couple
+{
+    public partial class FormGame : Form
+    {
+        public FormGame()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
